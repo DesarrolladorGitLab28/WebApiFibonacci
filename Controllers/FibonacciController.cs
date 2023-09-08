8f2d0a5d3fbc5f53e19d7524482e1eeb237415ed
Set Espejo <xespejos@clarios.com>
2023-09-07 21:49:38 -0600

    Proyecto cargado y terminado

---
+++ Controllers/FibonacciController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static WebApiFibonacci.Controllers.Fibonacci;
+
+namespace WebApiFibonacci.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FibonacciController : ControllerBase
+    {
+
+
+        public FibonacciController()
+        {
+
+        }
+
+        [HttpGet]
+        [Route("getFibonacci")]
+        public async Task<IActionResult> getFibonacci(int id)
+        {
+            Fibonacci _oper = new Fibonacci();
+            //return Ok(await _fibonacci.getFibonacci(id));
+            return Ok(await _oper.getFibonacci(id));
+
+        }
+
+
+
+    }
+
+    public class Fibonacci //: IFibonacci
+    {
+        public async Task<string> getFibonacci(int id)
+        {
+            string resultado = "0";
+
+            int a = 0, b = 1, c = 0;
+            for (int i = 2; i < id+1; i++)
+            {
+                c = a + b;
+                resultado = c.ToString();
+                a = b;
+                b = c;
+            }
+            return resultado;
+
+        }
+    }
+    
+}
